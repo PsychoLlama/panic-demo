8f7cdc174b0ffbe9f571267c49be38b21ba09811
Jesse Gibson <Jesse_Gibson@me.com>
2016-05-04 22:25:30 -0600

    Add boilerplate code, pretty index.html page
    The server dynamically adds your ip address to the index.html file so
    others can load your files without needing to change all the ip
    addresses themselves. By importing the code, the demo automatically
    starts the server on port 8080.

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/*eslint "no-console": "off"*/
+var panic = require('panic-server');
+
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var homepage = path.join(__dirname, 'index.html');
+var ip = require('ip').address();
+
+var server = http.Server(function (req, res) {
+	if (req.url === '/' || req.url === '/index.html') {
+		var file = fs.readFileSync(homepage, 'utf8');
+		file = file.replace(/{{IP_ADDRESS}}/g, ip);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(file);
+	}
+});
+
+panic.server(server).listen(8080, function () {
+	console.log('\n\nThe server is running!');
+	console.log('Open "http://localhost:8080/" in your browser');
+});
+
+var browsers = panic.clients.filter(function (client) {
+	return client.platform.name !== 'Node.js';
+});
+
+browsers.on('add', function (client) {
+	client = new panic.ClientList().add(client);
+	client.run(function () {
+		window.setView('connected', true);
+	});
+
+	function printTime() {
+		client.run(function () {
+			console.log(this.data.time);
+		}, {
+			time: new Date().toLocaleTimeString()
+		});
+	}
+
+	setInterval(printTime, 10000);
+	printTime();
+});
+
+module.exports = panic.clients;
